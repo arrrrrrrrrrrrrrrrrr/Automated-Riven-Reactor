f323d4cc1c02549711c7026b1e7e2f78af6b99a7
Arrrrr <11490236+arrrrrrrrrrrrrrrrrr@users.noreply.github.com>
2024-10-20 18:05:06 -0700

    Update setup_zurg_and_rclone.sh

--- setup_zurg_and_rclone.sh
+++ setup_zurg_and_rclone.sh
@@ -174,7 +174,7 @@ is_wsl() {
 # Check if running in WSL
 if is_wsl; then
     echo "Detected WSL environment."
-    VOLUME_OPTION="/mnt/zurg:/data:shared"
+    VOLUME_OPTION="/mnt/zurg:/data:rshared"
 else
     echo "Detected native Linux environment."
     VOLUME_OPTION="/mnt/zurg:/data:rshared"
